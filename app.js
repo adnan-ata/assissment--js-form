4725725b1f39513153f3551a5bf371e66ed34d59
adnan-ata <dareata@gmail.com>
2021-12-23 16:50:44 +0500

    changes

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 function getvalue (){
 
 var FirstNameinput = document.getElementById(FirstName);
-var a = FirstName.value
-// FirstNameinput.innerHTML = a;    
+var FirstName = FirstName.value
+console.log(FirstName1)
 
-console.log(a)
+var FatherName = document.getElementById(FatherName);
+var FatherName1 = FatherName.value
+console.log(FatherName1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+var date = document.getElementById(date);
+var date1 = date.value
+console.log(date1)
+
+
 return false;
 }
